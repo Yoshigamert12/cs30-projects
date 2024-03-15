cdb38a21afff3723be79920865d177fb7c5088cd
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-15 12:12:17 -0600

    FINALLY BUTTON DONE

--- Startscreen/sketch.js
+++ Startscreen/sketch.js
@@ -32,6 +32,10 @@ function draw() {
     //Calls the myButton function in the draw loop
     drawButton();
   }
+  else {
+    drawball();
+  }
+
 }
 
 //build in p5js funciton for mouse reactions
@@ -39,14 +43,25 @@ function mousePressed() {
 
   //Checks if the mouse is within the x coordinates of the button
   if (mouseX > myButton.x && mouseX < (myButton.x + myButton.w)) {
-    buttonPressed = true;
     //Switch "true" out for a similar condition that checks y coordinates
     if(mouseY > myButton.y && mouseY < (myButton.y + myButton.h)) {
       buttonPressed = true;
+      createball()
     }
+
     
   }else{
 buttonPressed=false;
+
+  }
+
+
+  if (buttonPressed = true) {
+    createball();
+    ballsarray[ballsarray.length-1].x + mouseX;
+   ballsarray[ballsarray.length-1].y + mouseY;
+
+
 
   }
 
@@ -57,67 +72,43 @@ buttonPressed=false;
 function drawButton() {
   fill(myButton.color);
   rect(myButton.x, myButton.y, myButton.w, myButton.h);
-}
-
-
-function Funy(){
-if (buttonPressed=false);
-  {
-  function setup() {
-    createCanvas(windowWidth, windowHeight);
-  createball();
-    
-    
   
-  }
-
-  function draw() {
-    background(30);
-
-
-    fill(255,0,0);
+}
+function drawball(){
+  fill(255,0,0);
   
 
-    for (let i=0; i< ballsarray.length; i++) {
-    circle(ballsarray[i].x, ballsarray[i].y, ballsarray[i].diameter);
-    ballsarray[i].x += ballsarray[i].dx;
-    ballsarray[i].y += ballsarray[i].dy;
-    }
-  }
-
-  function createball() {
-    let newBall = {
-      x: random(width),
-      y: random(height),
-      diameter: random(25, 100),
-      dx: random(-5, 5),
-      dy: random(-5, 5),
-
-
-
-
-
-
-
-  };
-
-  ballsarray.push(newBall)
-  }
-  function mousePressed() {
-  createball();
-    ballsarray[ballsarray.length-1].x + mouseX;
-    ballsarray[ballsarray.length-1].y + mouseY;
-
-  }
-
+  for (let i=0; i< ballsarray.length; i++) {
+ circle(ballsarray[i].x, ballsarray[i].y, ballsarray[i].diameter);
+ ballsarray[i].x += ballsarray[i].dx;
+ ballsarray[i].y += ballsarray[i].dy;
+ }
 
 
 
 }
 
 
+function createball() {
+  let newBall = {
+    x: random(width),
+   y: random(height),
+    diameter: random(25, 100),
+   dx: random(-5, 5),
+    dy: random(-5, 5),
 
 
 
 
+
+
+
+};
+
+ballsarray.push(newBall)
 }
+
+
+
+
+
