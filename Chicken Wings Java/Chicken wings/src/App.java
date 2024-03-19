93d2d6aabf4043abac3c17dd16acb3b28bd39b7c
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-19 12:10:37 -0600

    Started Chicken Wing

---
+++ Chicken Wings Java/Chicken wings/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    double ch;
+    ch = 4.75;
+   System.out.print(ch + " this is how much money you have for chicken wings");
+   double pr;
+   pr=.75;
+   System.out.print( pr + " this is how much 1 chicken wing costs ");
+
+   double tot;
+   tot= ch / pr ;
+   System.out.print( tot + " this is how many chicken wing you can get ");
+
+
+
+
+
+    }
+}
---
+++ Java Quizez/Quiz Testors/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int cost = 3.8;
+System.out.println("Price: " + cost);
+
+    }
+}
