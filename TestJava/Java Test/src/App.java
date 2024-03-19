c1e32bba4cbd7b31f5c543206989cddc430c049e
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-19 11:42:53 -0600

    Class Practice

--- TestJava/Java Test/src/App.java
+++ TestJava/Java Test/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ab\"\\c");
+        System.out.println("Hel\nlo");
+        System.out.println("Hel\tlo");
+        System.out.println("Hel\rlo");
+        System.out.println("Hel\blo");
+        System.out.println("Hel\\lo");
+        System.out.println("Hel\"lo");
+        System.out.println("Hel\'lo");
     }
 }
