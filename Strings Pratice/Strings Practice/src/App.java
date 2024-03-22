e1fcab2823ba72e5831765a39be292e55fd875f7
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-22 12:13:40 -0600

    Started Strings

---
+++ Strings Pratice/Strings Practice/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    String text2 = "hello fellas";
+
+    String text1 = " Fortnite is a no good game";
+    System.out.print (text1);
+
+
+    System.out.print(text1.substring( 0, 14));
+
+   
+
+    System.out.print(text1.substring(17, 27));
+
+    // System.out.print(text2.)
+
+
+
+
+
+
+
+    }
+}
