3b77333759f38a47b7022271c54971df25152ba9
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-25 12:08:45 -0600

    Scanner Practice

---
+++ Scanner Demo/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+      Scanner kebrd = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int num = kebrd.nextInt();
+    Scanner kebrd2 = new Scanner(System.in);
+        System.out.println("Enter a Snetance");
+        String sentence = kebrd2.nextLine();
+        System.out.println(num + " "+ sentence);
+
+
+
+
+
+
+    }
+}
