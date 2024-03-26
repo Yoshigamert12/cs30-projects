f13549df3218af6080e4a19724fda9eb0b011f66
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-26 11:52:30 -0600

    Classes and Methods Demo

---
+++ Classes Demo/src/Turkey.java
@@ -0,0 +1,15 @@
+public class Turkey {
+    
+    public void speak(){ 
+        System.out.println("Gobble-Gobble");
+    }
+    public void sayName() {
+        System.out.println("BigBird");
+        
+        speak();
+
+    }
+
+
+
+}
