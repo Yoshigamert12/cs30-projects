7277ea06765f48d71d86a7d321aff6b3ae5e3ffe
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-26 12:05:38 -0600

    Method 8b finish

---
+++ Method assignment/src/Goku.java
@@ -0,0 +1,34 @@
+public class Goku{ 
+
+    public void speak(){
+
+    System.out.println("Is that all you got Frieza?");
+    }
+    public void introduce (){
+
+        System.out.println("Hey it's me, Goku!");
+
+        speak();
+
+
+
+    }
+
+    public void blast(){
+        System.out.println(" KAHMEHAMEHA");
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Method assignment/src/GokuRunner.java
@@ -0,0 +1,13 @@
+public class GokuRunner {
+    public static void main(String[] args) throws Exception {
+        Goku guy= new Goku();
+        
+        guy.introduce();
+
+        guy.blast();
+
+
+
+
+    }
+}
