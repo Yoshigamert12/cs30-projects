ebf2193697afcf18cf518c745598972bbdc2e5c1
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-03-27 11:53:24 -0600

    Completed Assignment 8!!!! :D

---
+++ input basics/src/App.java
@@ -0,0 +1,91 @@
+
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+          {
+        
+              Scanner keyboard = new Scanner(System.in);
+        
+              int intOne, intTwo; 
+        
+              System.out.print("Enter an integer :: ");
+        
+              intOne = keyboard.nextInt();
+        
+         
+        
+              System.out.print("Enter an integer :: ");
+        
+              intTwo = keyboard.nextInt();
+        
+         
+        
+              System.out.println("Integer one = " + intOne);
+        
+              System.out.println("Integer two = " + intTwo);
+            
+              System.out.println( "Your New Integeter is " + (intOne + intTwo));
+
+           }
+           { 
+                Scanner keyboard = new Scanner(System.in);
+
+                double douOne, douTwo;
+
+                System.out.print("Enter a Decimal: ");
+
+                douOne = keyboard.nextDouble();
+
+
+                System.out.print("Enter a Decimal: ");
+
+                douTwo = keyboard.nextDouble();
+                
+                
+                System.out.println("Decimal one = " + douOne);
+        
+              System.out.println("Decimal two = " + douTwo);
+            
+              System.out.println( "Your New Decimal is " + (douOne + douTwo));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           }
+
+           
+        
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+    
+
+
+
+
