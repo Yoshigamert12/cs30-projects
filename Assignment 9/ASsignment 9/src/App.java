3fb9bfc97d1d18e01b63cd59d57f83c31be7ee96
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-04-08 11:24:23 -0600

    Learnt about Booleans and Conditionals

--- Assignment 9/ASsignment 9/src/App.java
+++ Assignment 9/ASsignment 9/src/App.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(7 + "" + 5);
-
+        System.out.println("Hello, World!");
     }
 }
---
+++ booleans practice/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       int x = 5;
+
+        if (x>5){
+            System.out.println("BIG NUMBER");
+
+        }
+        else if ( x > -12); {
+            System.out.println("Mild Weather");
+        }
+        
+        else {
+            System.out.println("VERY COLD");
+        }
+
+
+
+
+
+    }
+}
