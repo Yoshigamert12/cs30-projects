1a323be2f96aa9559c0c55481143b90837bac698
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-04-10 11:12:59 -0600

    finished the Assignment 9 :D

---
+++ A9 test/src/BigOrSmallrunner.java
@@ -0,0 +1,5 @@
+public class BigOrSmallrunner {
+    public static void main(String[] args) throws Exception {
+		BigOrSmall.check( 10, 20);
+    }
+}
---
+++ Assignment 9/ASsignment 9/src/BigOrSmallrunner.java
@@ -0,0 +1,14 @@
+public class BigOrSmallrunner {
+    public static void main(String[] args) throws Exception {
+		BigOrSmall.check( 10, 20);
+    BigOrSmall.check( 20, 10);
+    BigOrSmall.check( 20, 20);
+    BigOrSmall.check( 7, 90909390);
+    BigOrSmall.check( 678, 45);
+    BigOrSmall.check( 100000, 200000);
+    BigOrSmall.check( 6879, 34522);
+    BigOrSmall.check( 509450309, 509450309);
+    BigOrSmall.check( 2241, 410);
+    BigOrSmall.check( 778, 1000);
+    }
+}
