75979b3789d66cb5821c6b65ab84b9d01f5ad9e6
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-04-17 12:11:23 -0600

    Started Assignment 13

---
+++ Assignment 13 geomytry/src/geometry.java
@@ -0,0 +1,41 @@
+public class geometry{
+
+
+    public void perimeter(double len, double wid, double per){
+
+        len= 45;
+        wid=67.5;
+
+        per= (2*(len+wid));
+
+    }
+    
+    public void surfaceArea(double sid, double SA){
+
+        sid= 67;
+        SA= (6*(sid*67));
+
+
+    }
+
+
+    public void circleArea(double rad){
+
+        System.out.println("Print your answer here after calculating");
+        
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
