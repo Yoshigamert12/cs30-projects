cfb2ac9c2fb5ab4910099e7abe46d23b969f242a
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-04-23 12:00:45 -0600

    got a start on Area of a triangle

---
+++ Triangle Maker Assignment 14/src/Triangle.java
@@ -0,0 +1,56 @@
+import java.util.Scanner; 
+import java.lang.Math.*;
+
+public class Triangle
+{
+	private int sideA, sideB, sideC;
+	private double perimeter;
+	private double theArea;
+
+	public Triangle()
+	{
+	   setSides(0,0,0);
+	   perimeter=0;
+	   theArea=0;
+	}
+
+	public Triangle(int a, int b, int c)
+	{
+        
+
+
+	public void setSides(int a, int b, int c)
+	{
+        double sida;
+        double sidb;
+        double sidc;
+
+        sida=a;
+        sidb=b;
+        sidc=c;
+        
+	}
+
+	public void calcPerimeter( )
+	{
+        
+      perimeter=(a + b + c );
+
+	}
+
+	public void calcArea( )
+	{
+		double s ;
+        s = peri / 2;
+        double area;
+        area= sqrt(s(s–a)(s–b)(s–c))
+
+	}
+
+	public void print( )
+	{
+
+
+		System.out.println("\n\n");
+	}
+}
