d203bd771abcbde9860e52cab963181b71920ec4
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-05-08 12:05:52 -0600

    Started Stat Numbers :(

---
+++ Assignment 24 Loop stats/src/LoopStats.java
@@ -0,0 +1,39 @@
+import static java.lang.System.*;
+
+public class LoopStats
+{
+	private int start, stop;
+
+	public LoopStats()
+	{
+	}
+
+	public LoopStats(int beg, int end)
+	{
+	}
+
+	public void setNums(int beg, int end)
+	{
+        int num1 = beg;
+
+
+	}
+
+	public int checkEven()
+	{
+		int checkEven=0;
+        int num1;
+        if (num1 % 2 == 1 )
+
+
+
+		return checkEven;
+	}
+
+	
+	
+	public String toString()
+	{
+		return start + " " + stop;
+	}
+}
---
+++ Assignment 24 Loop stats/src/LoopStatsRunner.java
@@ -0,0 +1,22 @@
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+public class LoopStatsRunner
+{
+	public static void main ( String[] args )
+	{
+		Scanner keys = new Scanner(System.in);
+        LoopStats lib = new LoopStats();
+
+        System.out.println("Enter your first number: ");
+        int num1=keys.nextLine();
+
+        System.out.println("Enter Your Second Number: ");
+        int num2 = keys.nextLine();
+
+
+
+
+	}
+}
