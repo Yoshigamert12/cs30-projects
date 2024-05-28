6f3ac24f45536d64ff840222cc0a80072d2f8d35
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-05-28 12:05:26 -0600

    Final Stuff

--- FinalProject/src/MainGame.java
+++ FinalProject/src/MainGame.java
@@ -6,7 +6,11 @@ public class MainGame {
        String choice;
 
        System.out.println("Welcome young Miller Student into a game of Average Miller Day on this adventure you are to choose a number on your keyboard corrisponding to the choice Have fun");
-        System.out.print("You wake up on an average Monday Morning you get ready and head to Miller, your high school, you walk into miller would you like to. 1. Head to class. 2. use the wash room. 3.leave")
-       
+        System.out.print("You wake up on an average Monday Morning you get ready and head to Miller, your high school, you walk into miller would you like to. 1. Head to class. 2. use the wash room. 3.leave");
+       choice =sc.nextLine();
+       if (choice == "1");{
+        
+       }
+
     }
 }
--- Grocery List/src/App.java
+++ Grocery List/src/App.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class App {
-    public  main(String args[]){
-   
+    public static void main(String[] args) throws Exception {
+    Scanner keys = new Scanner (System.in);
+    
+        String [] item = {};
+        int added;
+        System.out.println("what would you like to add to your list");
+        added = keys.nextInt();
+        System.out.println("Your list " + item[added]);
+    }
 
 }
